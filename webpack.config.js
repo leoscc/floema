24e393b13d860bb2858d0775eccdf5f654843641
Leonardo Carvalho <leonardoscc15@gmail.com>
2023-03-13 23:51:01 -0300

    [Setup] setup plugins for js,scss,images and fonts and remove dirAssets

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,10 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
 
 const dirApp = path.join(__dirname, 'app')
-const dirAssets = path.join(__dirname, 'assets')
 const dirStyles = path.join(__dirname, 'styles')
 const dirNode = 'node_modules'
 
-console.log(dirApp, dirAssets, dirStyles)
+console.log(dirApp, dirStyles)
 
 module.exports = {
   entry: [
@@ -21,9 +20,66 @@ module.exports = {
 
   resolve: {
     modules: [
-      dirApp, 
+      dirApp,       
       dirStyles,
       dirNode
     ],
+  },
+
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "./shared", to: "" },
+      ],
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        },
+      },
+
+      // styles
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: ''
+            }
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: "postcss-loader",
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ],
+      },
+
+      // images and fonts
+      {
+        test: /\.(jpe?g|png|gif|svg|avf|woff2?|fnt|webp)$/,
+        loader: 'file-loader',
+        options: {
+          name (file) {
+            return '[name].[hash].[ext]'
+          }
+        }
+      }
+    ]
   },
 }
