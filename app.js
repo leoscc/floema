444c1fc611515896d6f3cff956d9fb9c9b14a26b
Leonardo Carvalho <leonardoscc15@gmail.com>
2023-03-20 23:18:30 -0300

    [Project Structure] update express to handle with pug

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 const PORT = 3000
 
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index', {
+    meta: {
+      data: {
+        title: 'title',
+        description: 'description'
+      }
+    }
+  })
 })
 
 app.listen(PORT, () => {
